ddf73fd6047448e5bee6238bcb85870cab119528
Vinicius Mesel <vinicius@bigsp.com.br>
2016-11-02 10:11:54 -0200

    Create variaveis.py

---
+++ src/variaveis.py
@@ -0,0 +1,13 @@
+inteiro = 2
+texto = "Ola Mundo"
+decimal = 0.00
+tupla = ("Ola", "Mundo")
+dicionario = {"Ola": "Mundo"}
+lista = ["primeiro item", "segundo item"]
+
+print("Inteiro - Conteudo da Variavel: {} e tipo da variavel {}".format(inteiro,type(inteiro)))
+print("Texto - Conteudo da Variavel: {} e tipo da variavel {}".format(texto,type(texto)))
+print("Decimal - Conteudo da Variavel: {} e tipo da variavel {}".format(decimal,type(decimal)))
+print("Tupla - Conteudo da Variavel: {} e tipo da variavel {}".format(tupla,type(tupla)))
+print("Dicionario - Conteudo da Variavel: {} e tipo da variavel {}".format(dicionario,type(dicionario)))
+print("Lista - Conteudo da Variavel: {} e tipo da variavel {}".format(lista,type(lista)))
